bb1ca2f1c3265571bdc73bb0469c23cb2dd5fe36
ValyaTsakunova <vtsakunova@mail.ru>
2021-10-06 13:43:38 +0300

    add multiselector

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
+import React, {useState} from 'react';
+import MultiSelect from './MultiSelect';
 
 function AdminPage(){
-    
+    const [selected, setSelected] = useState([]);
+    const options = [
+        { value: "1", label: "Jimmy" },
+        { value: "2", label: "Laura" },
+        { value: "3", label: "Tommy" },
+        { value: "4", label: "Jane" },
+        { value: "5", label: "Mike" }
+    ];
     return(
         <div >
-           AdminPage
+           <MultiSelect options={options} value={selected} onChange={setSelected} />
         </div>
     )
 }
